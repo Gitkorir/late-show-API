b98a2b9cb3f261bf7e414440b3466464028b7ab2
Arnold <arnoldkorir201@gmail.com>
2025-06-23 18:33:13 +0300

    create a basecontoller to handle shared logic

---
+++ server/controllers/base_controller.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from server.app import db
+from flask.views import MethodView
+from werkzeug.exceptions import NotFound
+
+class Basecontoller(MethodView):
+
+    model = None #to be set in child class
+
+    def get(self, id=None):
+        if id is None:
+            records = self.model.query.all()
+            return jsonify([r.to_dict() for r in records]), 200
+        else:
+            record = self.model.query.get(id)
+            if not record :
+                raise NotFound(f"{self.model.__name__} with {id} not found.")
+            return jsonify(record.to_dict()), 200
+        
+    def delete(self,id):
+        record =self.model.query.get(id)
+        if not record:
+            raise NotFound(f'{self.model.__name__} with {id} not found.')
+        db.session.delete(record)
+        db.session.commit()
+        return jsonify({"message": f"{self.model.__name__} deleted successfuly."}), 204    
