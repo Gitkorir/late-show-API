85aec3f64e3253ad79948068cf605024c52b9d1e
Arnold <arnoldkorir201@gmail.com>
2025-06-24 16:00:45 +0300

    define a username tabke schema, handles password hashing

--- server/models/user.py
+++ server/models/user.py
@@ -1,5 +1,5 @@
 from server.extensions import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__='users'
@@ -7,3 +7,15 @@ class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(100), unique=True, nullable=False)
     password_harsh=db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_harsh= generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_harsh,password)  
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'username':self.username
+        }  
